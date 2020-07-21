924982f5531cb7d2e3c130e0106afd24256ec34a
Anthony Curtis <tonydc1997@gmail.com>
2020-07-21 11:19:16 -0400

    feat: Output response to test running server
    - fix: Ensure `port: PORT`

--- mod.ts
+++ mod.ts
@@ -3,6 +3,10 @@ import { Application } from "https://deno.land/x/oak@v6.0.1/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
+app.use((context) => {
+  context.response.body = `Hello World!`;
+});
+
 app.listen({
-  PORT,
+  port: PORT,
 });
