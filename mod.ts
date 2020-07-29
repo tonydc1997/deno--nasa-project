9132d25c9e7893b2499a35fe383c6508402e3cb2
Anthony Curtis <tonydc1997@gmail.com>
2020-07-29 11:05:05 -0400

    feat: Ensure middleware goes to next one

--- mod.ts
+++ mod.ts
@@ -3,7 +3,9 @@ import { Application } from "https://deno.land/x/oak@v6.0.1/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
-app.use(async (ctx, next) => {});
+app.use(async (ctx, next) => {
+  await next();
+});
 
 app.use((ctx) => {
   ctx.response.body = `Hello World!`;
