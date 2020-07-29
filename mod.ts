806237cd554d3da5e973848dea4add28ece132bd
Anthony Curtis <tonydc1997@gmail.com>
2020-07-29 11:04:26 -0400

    feat: Rename arg for doc consistency

--- mod.ts
+++ mod.ts
@@ -3,10 +3,10 @@ import { Application } from "https://deno.land/x/oak@v6.0.1/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
-app.use(async (context, next) => {});
+app.use(async (ctx, next) => {});
 
-app.use((context) => {
-  context.response.body = `Hello World!`;
+app.use((ctx) => {
+  ctx.response.body = `Hello World!`;
 });
 
 if (import.meta.main) {
