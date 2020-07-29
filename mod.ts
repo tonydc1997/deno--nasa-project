9b5c170f638f81476281c9c845839786fd373b28
Anthony Curtis <tonydc1997@gmail.com>
2020-07-29 11:03:58 -0400

    feat: Create basic async middleware

--- mod.ts
+++ mod.ts
@@ -3,6 +3,8 @@ import { Application } from "https://deno.land/x/oak@v6.0.1/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
+app.use(async (context, next) => {});
+
 app.use((context) => {
   context.response.body = `Hello World!`;
 });
